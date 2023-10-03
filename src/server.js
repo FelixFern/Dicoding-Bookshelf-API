8b2a31b8d686b6afbc730d0a1a5c3ac86d8dbdd1
felixfern <felixzhuang1@gmail.com>
2023-10-04 00:40:29 +0700

    feat: update host ip and port ip

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const { getBooksRoutes } = require("./route/getBooks");
 const { updateBooksById } = require("./route/updateBooks");
 const { deleteBooksById } = require("./route/deleteBooks");
 
-const PORT = 22;
-const HOST = "0.0.0.0";
+const PORT = 80;
+const HOST = "20.2.81.27";
 
 const init = async () => {
 	const server = hapi.server({
