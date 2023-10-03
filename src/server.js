456660d124c47b844f1e9354b8ca40ff4f1268a1
felixfern <felixzhuang1@gmail.com>
2023-10-04 00:41:20 +0700

    feat: updated port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const { getBooksRoutes } = require("./route/getBooks");
 const { updateBooksById } = require("./route/updateBooks");
 const { deleteBooksById } = require("./route/deleteBooks");
 
-const PORT = 80;
+const PORT = 3000;
 const HOST = "20.2.81.27";
 
 const init = async () => {
