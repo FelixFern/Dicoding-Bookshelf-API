036415d1179c9ee1ee09047f47ff4606e7eeddef
felixfern <felixzhuang1@gmail.com>
2023-10-04 00:32:09 +0700

    feat: update host ip and port ip

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const { getBooksRoutes } = require("./route/getBooks");
 const { updateBooksById } = require("./route/updateBooks");
 const { deleteBooksById } = require("./route/deleteBooks");
 
-const PORT = 9000;
-const HOST = "localhost";
+const PORT = 80;
+const HOST = "20.2.81.27";
 
 const init = async () => {
 	const server = hapi.server({
@@ -14,7 +14,7 @@ const init = async () => {
 	});
 
 	await server.start();
-	console.log(`Server is listnening on port ${HOST}`);
+	console.log(`Server is listening on port ${HOST}`);
 
 	server.route([
 		...getBooksRoutes,
